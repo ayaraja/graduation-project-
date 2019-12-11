f7f1754ea9fcf81988b8c441975520498370b718
pc-moon programmer and paramedic <pc-moon@homail.com>
2019-12-11 03:04:16 +0300

    DB Search

--- grpr22/ggpp/gp/gpApp/src/app/cafelist/cafelist.page.ts
+++ grpr22/ggpp/gp/gpApp/src/app/cafelist/cafelist.page.ts
@@ -4,18 +4,41 @@ import { NavController, LoadingController } from '@ionic/angular';
 import { formatDate } from '@angular/common';
 import { cafes, CafesService } from './../services/Cafes.service';
 
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+
+
+export interface cafes {
+  id?: string;
+  name: string;
+  type_place: string;
+  type_j: string;
+  type_cat: string;
+  vistor_Type: string;
+  vistor_Type1: string;
+  Date: string;
+  Time_start: string;
+  Time_end: string;
+  cost: string;
+  number_vist: number;
+  rate: number;
+}
+
 @Component({
   selector: 'app-cafelist',
   templateUrl: './cafelist.page.html',
   styleUrls: ['./cafelist.page.scss'],
 })
+
 export class CafelistPage implements OnInit {
 
   public cafe: cafes[];
 
 
-
+  private cafesCollection: AngularFirestoreCollection<cafes>;
+  private cafes: Observable<cafes[]>;
 
 
   constructor(private route: ActivatedRoute,
@@ -28,7 +51,19 @@ export class CafelistPage implements OnInit {
   FilterJSONData(ev: any) {
     const val = ev.target.value;
     console.log(val)
-    const cafes = this.db.collection("cafes", ref => ref.where("name", "==",val));
+    const cafes = this.db.collection<cafes>("cafes", ref => ref.where("name", "==",val));
+    this.cafes = cafes.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+    this.cafes.subscribe(res => {
+      this.cafe = res;
+    })
  
   }
 
